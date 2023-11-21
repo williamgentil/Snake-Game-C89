05a012e1ebb0fd0bc298742eb107aa1f3ed71bfb
Votre nom <william.gentil@etu.u-pec.fr>
2023-11-21 16:41:09 +0100

    https://gentil@dwarves.iut-fbleau.fr

--- progressbar.c
+++ progressbar.c
@@ -23,56 +23,11 @@ int main() {
 
     for (int i = 0; i <= total; ++i) {
         afficherBarreDeChargement(total, i);
-        usleep(100000); // Pause de 100 000 microsecondes (0.1 seconde)
+        usleep(30000); // Pause de 100 000 microsecondes (0.1 seconde)
     }
     printf("\nChargement terminé!\n");
 
     return 0;
-    /*void progressbar(int percentage) {
-    printf("[");
-    int i;
-    for (i = 0; i < percentage / 2; i++) {
-        printf("\U+0001F40D"); 
-    }
-    for (i; i < 50; i++) {
-        printf(" ");
-    }
-    printf("] %d%%\r", percentage);
-    fflush(stdout);
-}
-
-int main() {
-    int percentage = 0;
-
-
-    while (percentage <= 100) {
-        progressbar(percentage);
-        usleep(30000); 
-        percentage += 2; 
-    }
-
-    printf("\nLancement du jeu\n");
-    printf("Le logo de notre jeu : \n\n\n")
-    printf("/^\\/^\\ 
-         _|__|  O|
-\\/     /~    \\_/ \
- \\____|__________/  \
-       \\_______      \
-                `\\     \\                 \
-                  |     |                  \
-                 /      /                    \
-                /     /                       \\
-              /      /                        \\ \
-             /     /                           \\  \
-           /     /             _----_           \\   \
-          /     /           _-~      ~-_         |   |
-         (      (        _-~    _--_    ~-_     _/   |
-        \\      ~-____-~    _-~    ~-_    ~-_-~    /
-            ~-_           _-~          ~-_       _-~
-               ~--______-~                ~-___-~")
-
-    return 0;
-}*/
 }
 
 
