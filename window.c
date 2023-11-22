aebb413f28142bc3a0e1eb6e75c8db7045cd7e32
Votre nom <william.gentil@etu.u-pec.fr>
2023-11-22 10:54:00 +0100

    https://dwarves.iut-fbleau.fr/gitiut/gentil/SAE11_2023.git

--- window.c
+++ window.c
@@ -7,30 +7,19 @@ int main()
    FILE* apple;
    apple=fopen("item 2-1.png.png", "r");
    int Length=60, Heigth=40;
-   int WindowMargin=10, WindowLength=30*Length, WindowHeigth=30*Heigth;
+   int WindowMargin=10, WindowLength=32*Length, WindowHeigth=25.5*Heigth;
+   int GameScreenMarginX=Length, GameScreenMarginY=WindowMargin+Heigth;
+   int GameScreenLength=WindowLength-2*GameScreenMarginX, GameScreenHeigth=WindowHeigth-3*GameScreenMarginY;
    InitialiserGraphique();
    CreerFenetre(WindowMargin, WindowMargin, WindowLength, WindowHeigth);
    couleur c;
    c=CouleurParNom("black");
    EffacerEcran(c);
    ChoisirCouleurDessin(CouleurParNom("white"));
-   EcrireTexte(WindowLength/3, Heigth, "Snake", 2);
-   ChoisirCouleurDessin(CouleurParComposante(110, 207,39));
-   RemplirRectangle(20, 50, 1240, 800);
+   EcrireTexte(WindowLength/2.15, Heigth, "Snake", 2);
+   ChoisirCouleurDessin(CouleurParComposante(157, 207, 31));
+   RemplirRectangle(GameScreenMarginX, GameScreenMarginY, GameScreenLength, GameScreenHeigth);
    Touche();
    FermerGraphique();
    return EXIT_SUCCESS;
-   //Fonction temps en temps réel 
-   /*on marque le debut du cycle 
-   suivant= Microsecondes()+CYCLE;
-   while (...) /* boucle principale du programme 
-   {
-	if (Microsecondes()>suivant)
-	{
-		/* une periode s'est écoulé 
-		/* prochaine date 
-		suivant= Microsecondes()+CYCLE;
-
-}
-   }*/
 }
