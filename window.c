673af56abf4114888ff39af4fbabc0576cc1136a
William Gentil <william.gentil@etu.u-pec.fr>
2023-11-28 16:16:03 +0100

    window.c

--- window.c
+++ window.c
@@ -1,11 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <graph.h>
-
-int main()
-{ 
-   FILE *apple;
-   char apple_sprite[]="item 2-1.png.png";
+void window(){
    int Length=60, Heigth=40;
    int WindowMargin=10, WindowLength=32*Length, WindowHeigth=25.5*Heigth;
    int GameScreenMarginX=Length, GameScreenMarginY=WindowMargin+Heigth;
@@ -19,10 +15,20 @@ int main()
    EcrireTexte(WindowLength/2.15, Heigth, "Snake", 2);
    ChoisirCouleurDessin(CouleurParComposante(157, 207, 31));
    RemplirRectangle(GameScreenMarginX, GameScreenMarginY, GameScreenLength, GameScreenHeigth);
-   apple=fopen("Apple_JE3_BE3.png", "r");
-   ChargerSprite(FILE *apple); //<- Pose un problème pour l'instant
+   Touche();
+   FermerGraphique();
+   return;
+}
+void apple(){
+   FILE *apple;
+   apple=fopen("Apple_JE3_BE3.png", "w");
+   ChargerImage(apple, 0, 0, 0, 0, 0, 0); //<- Pose un problème pour l'instant
    Touche();
    fclose(apple);
-   FermerGraphique();
-   return EXIT_SUCCESS;
+   return;
 }
+int main(void)
+{ 
+   return 0;
+}
+   
